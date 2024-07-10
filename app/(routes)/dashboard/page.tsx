c9554a0cb94452be5e19eeba1e5a6f75b2ff180e
Nikith-S <nikith391@gmail.com>
2024-07-11 03:20:38 +0530

    convex database is implemented

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,10 +1,32 @@
 "use client"
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import {Button } from '@/components/ui/button'
-import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs'
+import { LogoutLink, useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import { useMutation, useQuery } from 'convex/react'
+import { api } from '@/convex/_generated/api'
 
 function Dashboard() {
+
+const {user}:any = useKindeBrowserClient();
+  const getUser = useQuery(api.user.getUser,{email:user?.email})
+const createUser = useMutation(api.user.createUser)
+
+useEffect(() => {
+if(user){
+ if(getUser=== undefined){
+  createUser({
+    name:user.given_name,
+    email:user.email,
+    image:user.picture
+  }).then(resp => {
+    console.log(resp)
+  })
+ }
+}
+ 
+},[user])
+
   return (
     <div>
       Dashboard
@@ -17,3 +39,6 @@ function Dashboard() {
 }
 
 export default Dashboard
+
+
+
