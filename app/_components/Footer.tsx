e54367eda38951d5520a6c3ed4beb3c52df7d7ef
Nikith-S <nikith391@gmail.com>
2024-07-12 23:25:07 +0530

    footer is created

---
+++ app/_components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      Footer is implemented
+      <div>
+        About us is created
+      </div>
+    </div>
+  )
+}
+
+export default Footer
