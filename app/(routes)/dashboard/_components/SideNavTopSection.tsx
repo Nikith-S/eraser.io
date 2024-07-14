7cb03680c94936ff6deaef0e0c1a6a96d818f8ef
Nikith-S <nikith391@gmail.com>
2024-07-14 19:15:11 +0530

    navigation of routes for create team is created

--- app/(routes)/dashboard/_components/SideNavTopSection.tsx
+++ app/(routes)/dashboard/_components/SideNavTopSection.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { ChevronDown, Users, Settings, LogOut } from 'lucide-react';
-import {api} from '@/convex/_generated/api';
+import { api } from '@/convex/_generated/api';
 import {
     Popover,
     PopoverContent,
@@ -10,8 +10,15 @@ import {
 import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs';
 import { Separator } from '@/components/ui/separator';
 import { useConvex } from 'convex/react';
+import { useRouter } from 'next/navigation';
 
-function SideNavTopSection({user}) {
+export interface Team {
+    createdBy: string,
+    teamName: string,
+    _id: string
+}
+
+function SideNavTopSection({ user }) {
     const menu = [
         {
             id: 1,
@@ -26,29 +33,48 @@ function SideNavTopSection({user}) {
             icon: Settings
         }
     ];
- 
 
     useEffect(() => {
-        user&& getTeamList();
-        
-    },[user])
+        user && getTeamList();
+    }, [user]);
+
     const convex = useConvex();
-    const getTeamList= async() => {
-        const result = await convex.query(api.teams.getTeam, {email:user.email})
-        console.log("TeamList", result)
+
+    const [teamList, setTeamList] = useState<Team[]>([]);
+
+    const [activeTeam, setActiveTeam] = useState<Team | null>(null);
+
+    const router = useRouter()
+
+    const getTeamList = async () => {
+        const result = await convex.query(api.teams.getTeam, { email: user.email });
+        console.log("TeamList", result);
+        setTeamList(result);
+        setActiveTeam(result[0]);
+    };
+
+
+    const onMenuClick= (item:any) => {
+
+        if(item?.path){
+
+            router.push(item.path)
+        }
+
     }
+
     return (
         <Popover>
-            <PopoverTrigger>  
+            <PopoverTrigger>
                 <div className="flex items-center gap-3 hover:bg-slate-200 p-3 rounded-md cursor-pointer">
-                    <Image 
-                        src='/logo-1.png' 
-                        alt='logo' 
-                        width={40} 
+                    <Image
+                        src='/logo-1.png'
+                        alt='logo'
+                        width={40}
                         height={40}
                     />
                     <h2 className='flex gap-2 items-center font-bold text-[17px]'>
-                        Team Name
+                        {activeTeam?.teamName}
                         <ChevronDown />
                     </h2>
                 </div>
@@ -56,43 +82,47 @@ function SideNavTopSection({user}) {
             <PopoverContent className='ml-7 p-4'>
                 {/* Team Section */}
                 <div>
-                    <h2>
-                        Team Name
-                    </h2>
+                    {teamList.map((team) => (
+                        <h2 key={team._id} className={`p-2 hover:bg-blue-500 hover:text-white rounded-lg mb-1 cursor-pointer ${activeTeam?._id === team._id ? 
+                          'bg-blue-500 text-white' : ''}`} onClick={() => setActiveTeam(team)}>
+                            {team.teamName}
+                        </h2>
+                    ))}
                 </div>
-                <Separator className='mt-2 bg-slate-100'/>
+                <Separator className='mt-2 bg-slate-100' />
                 {/* Option Section */}
                 <div>
-                    {menu.map((item, idex) => (
-                        <h2 className='flex gap-2 items-center p-2 hover:bg-gray-100 rounded-lg cursor-pointer'>
-                            <item.icon className='h-4 w-4'/>
-                            {item.name}</h2>
+                    {menu.map((item) => (
+                        <h2 key={item.id} className='flex gap-2 items-center p-2 hover:bg-gray-100 rounded-lg cursor-pointer' onClick={() => onMenuClick(item)}>
+                            <item.icon className='h-4 w-4' />
+                            {item.name}
+                        </h2>
                     ))}
                     <LogoutLink>
-                    <h2 className='flex gap-2 items-center p-2 hover:bg-gray-100 rounded-lg cursor-pointer'>
-                            <LogOut className='h-4 w-4'/>
-                            Logout</h2>
-                            </LogoutLink>
+                        <h2 className='flex gap-2 items-center p-2 hover:bg-gray-100 rounded-lg cursor-pointer'>
+                            <LogOut className='h-4 w-4' />
+                            Logout
+                        </h2>
+                    </LogoutLink>
+                </div>
+                <Separator className='mt-2 bg-slate-100' />
 
-                </div>
-                <Separator className='mt-2 bg-slate-100'/>
-        
-            {/* userInfo */}
-          
-                {user &&   
-                <div className='mt-2 flex gap-2 items-center'>
-                <Image src={user?.picture} alt='user'
-                width={30}
-                height={30} 
-                className='rounded-full' />
-                <div>
-                    <h2 className='text-[14px] font-bold'>{user?.given_name}{user?.family_name}</h2>
-                    <h2 className='text-[12px] font-gray-500'>{user?.email}</h2>
-                </div>
-            </div>  
-            }
+                {/* User Info */}
+                {user &&
+                    <div className='mt-2 flex gap-2 items-center'>
+                        <Image src={user?.picture} alt='user'
+                            width={30}
+                            height={30}
+                            className='rounded-full' />
+                        <div>
+                            <h2 className='text-[14px] font-bold'>{user?.given_name} {user?.family_name}</h2>
+                            <h2 className='text-[12px] text-gray-500'>{user?.email}</h2>
+                        </div>
+                    </div>
+                }
             </PopoverContent>
         </Popover>
     );
 }
+
 export default SideNavTopSection;
