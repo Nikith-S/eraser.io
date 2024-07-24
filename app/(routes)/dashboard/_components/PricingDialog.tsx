bbab4e248d611be12daa11f77e13600f3d06a11f
Nikith-S <nikith391@gmail.com>
2024-07-24 23:48:43 +0530

    pricing dialog is created

---
+++ app/(routes)/dashboard/_components/PricingDialog.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import React from 'react'
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+import Link from 'next/link'
+
+const PricingDialog = () => {
+  return (
+    <div>
+      pricing dialog 
+
+    </div>
+    
+  )
+}
+
+export default PricingDialog
