41760ca14b3d3f5bd38046de3de9b4f823047b54
Nikith-S <nikith391@gmail.com>
2024-07-28 10:21:52 +0530

    workspace layout is created

--- app/(routes)/workspace/[fileId]/page.tsx
+++ app/(routes)/workspace/[fileId]/page.tsx
@@ -5,6 +5,15 @@ const Workspace = () => {
   return (
     <div>
       <WorkSpaceHeader />
+
+      {/* WorkspaceLayout  */}
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        {/* Document  */}
+        <div className="bg-blue-400 h-screen">document</div>
+
+        {/* whiteBoardcanvas  */}
+        <div className="bg-red-400 h-screen">canvas</div>
+      </div>
     </div>
   );
 };
--- app/(routes)/workspace/_components/WorkSpaceHeader.tsx
+++ app/(routes)/workspace/_components/WorkSpaceHeader.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Link } from "lucide-react";
 
 const WorkSpaceHeader = () => {
   return (
-    <div>
-      <Image src={"/logo-1.png"} alt="logo" height={40} width={40} />
-      <h2>File Name</h2>
+    <div className="p-3 border-b">
+      <div className="flex gap-2 items-center">
+        <Image src={"/logo-1.png"} alt="logo" height={40} width={40} />
+        <h2>File Name</h2>
+      </div>
+      <Button className="h-8 text-[12px] gap-2 bg-blue-600 hover:bg-blue-700">
+        share <Link className="h-4 w-4" />
+      </Button>
     </div>
   );
 };
