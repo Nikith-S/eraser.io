4d381e9b8d3e84677716486a5c4f31dce67300fe
Nikith-S <nikith391@gmail.com>
2024-08-14 14:13:10 +0530

    commit

--- app/(routes)/workspace/[fileId]/page.tsx
+++ app/(routes)/workspace/[fileId]/page.tsx
@@ -1,24 +1,55 @@
-import React from "react";
-import WorkSpaceHeader from "../_components/WorkSpaceHeader";
+"use client";
+import React, { useEffect, useState } from "react";
+import WorkspaceHeader from "../_components/WorkspaceHeader";
 import Editor from "../_components/Editor";
+import { useConvex } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { FILE } from "../../dashboard/_components/FileList";
+import Canvas from "../_components/Canvas";
 
-const Workspace = () => {
+function Workspace({ params }: any) {
+  const [triggerSave, setTriggerSave] = useState(false);
+  const convex = useConvex();
+  const [fileData, setFileData] = useState<FILE | any>();
+  useEffect(() => {
+    console.log("FILEID", params.fileId);
+    params.fileId && getFileData();
+  }, []);
+
+  const getFileData = async () => {
+    const result = await convex.query(api.files.getFileById, {
+      _id: params.fileId,
+    });
+    setFileData(result);
+  };
   return (
     <div>
-      <WorkSpaceHeader />
+      <WorkspaceHeader onSave={() => setTriggerSave(!triggerSave)} />
 
-      {/* WorkspaceLayout  */}
-      <div className="grid grid-cols-1 md:grid-cols-2">
+      {/* Workspace Layout  */}
+      <div
+        className="grid grid-cols-1
+      md:grid-cols-2"
+      >
         {/* Document  */}
         <div className=" h-screen">
-          <Editor />
+          <Editor
+            onSaveTrigger={triggerSave}
+            fileId={params.fileId}
+            fileData={fileData}
+          />
         </div>
-
-        {/* whiteBoardcanvas  */}
-        <div className="bg-red-400 h-screen">canvas</div>
+        {/* Whiteboard/canvas  */}
+        <div className=" h-screen border-l">
+          <Canvas
+            onSaveTrigger={triggerSave}
+            fileId={params.fileId}
+            fileData={fileData}
+          />
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default Workspace;
--- app/(routes)/workspace/_components/Editor.tsx
+++ app/(routes)/workspace/_components/Editor.tsx
@@ -1,23 +1,61 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import EditorJS from "@editorjs/editorjs";
+// @ts-ignore
 import Header from "@editorjs/header";
+// @ts-ignore
+import List from "@editorjs/list";
+// @ts-ignore
+import Checklist from "@editorjs/checklist";
+// @ts-ignore
+import Paragraph from "@editorjs/paragraph";
+// @ts-ignore
+import Warning from "@editorjs/warning"; // Import Warning if needed, otherwise remove
 
-const Editor = () => {
+const Editor = ({ fileData, rawDocument }) => {
+  const editorInstance = useRef(null);
+
   useEffect(() => {
+    const initEditor = () => {
+      const editor = new EditorJS({
+        holder: "editorjs",
+        tools: {
+          header: {
+            class: Header,
+            shortcut: "CMD+SHIFT+H",
+            config: {
+              placeholder: "Enter a Header",
+            },
+          },
+          list: {
+            class: List,
+            inlineToolbar: true,
+            config: {
+              defaultStyle: "unordered",
+            },
+          },
+          checklist: {
+            class: Checklist,
+            inlineToolbar: true,
+          },
+          paragraph: Paragraph,
+          warning: Warning, // Make sure this is intended or remove if not used
+        },
+        data: fileData?.document ? JSON.parse(fileData.document) : rawDocument,
+      });
+
+      editorInstance.current = editor;
+    };
+
     initEditor();
-  }, []);
-  const initEditor = () => {
-    const editor = new EditorJS({
-      /**
-       * Id of Element that should contain Editor instance
-       */
-      tools: {
-        header: Header,
-      },
-      holder: "editorjs",
-    });
-  };
+
+    return () => {
+      if (editorInstance.current) {
+        editorInstance.current.destroy();
+        editorInstance.current = null;
+      }
+    };
+  }, [fileData, rawDocument]);
 
   return (
     <div>
