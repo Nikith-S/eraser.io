391e38d4489720d7373b809d229b527725a6d2d4
Nikith-S <nikith391@gmail.com>
2024-08-23 08:34:53 +0530

    add checklist for the editor page and many other features

--- app/(routes)/workspace/_components/Editor.tsx
+++ app/(routes)/workspace/_components/Editor.tsx
@@ -1,23 +1,49 @@
 "use client";
 import React, { useEffect } from "react";
 import EditorJS from "@editorjs/editorjs";
+// @ts-ignore
 import Header from "@editorjs/header";
+// @ts-ignore
+import List from "@editorjs/list";
+// @ts-ignore
+import Checklist from "@editorjs/checklist";
+// @ts-ignore
+import Paragraph from "@editorjs/paragraph";
+// @ts-ignore
+import Warning from "@editorjs/warning";
 
 const Editor = () => {
   useEffect(() => {
+    const initEditor = () => {
+      const editor = new EditorJS({
+        holder: "editorjs",
+        tools: {
+          header: {
+            class: Header,
+            shortcut: "CMD+SHIFT+H",
+            config: {
+              placeholder: "Enter a Header",
+            },
+          },
+          list: {
+            class: List,
+            inlineToolbar: true,
+            config: {
+              defaultStyle: "unordered",
+            },
+          },
+          checklist: {
+            class: Checklist,
+            inlineToolbar: true,
+          },
+          paragraph: Paragraph,
+          warning: Warning,
+        },
+      });
+    };
+
     initEditor();
   }, []);
-  const initEditor = () => {
-    const editor = new EditorJS({
-      /**
-       * Id of Element that should contain Editor instance
-       */
-      tools: {
-        header: Header,
-      },
-      holder: "editorjs",
-    });
-  };
 
   return (
     <div>
