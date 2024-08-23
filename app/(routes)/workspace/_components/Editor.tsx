a1a31d379263b5ab339f8afa755cd83bec874042
Nikith-S <nikith391@gmail.com>
2024-08-23 07:41:30 +0530

    Revert "commit"
    This reverts commit 4d381e9b8d3e84677716486a5c4f31dce67300fe.

--- app/(routes)/workspace/_components/Editor.tsx
+++ app/(routes)/workspace/_components/Editor.tsx
@@ -1,61 +1,23 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import EditorJS from "@editorjs/editorjs";
-// @ts-ignore
 import Header from "@editorjs/header";
-// @ts-ignore
-import List from "@editorjs/list";
-// @ts-ignore
-import Checklist from "@editorjs/checklist";
-// @ts-ignore
-import Paragraph from "@editorjs/paragraph";
-// @ts-ignore
-import Warning from "@editorjs/warning"; // Import Warning if needed, otherwise remove
 
-const Editor = ({ fileData, rawDocument }) => {
-  const editorInstance = useRef(null);
-
+const Editor = () => {
   useEffect(() => {
-    const initEditor = () => {
-      const editor = new EditorJS({
-        holder: "editorjs",
-        tools: {
-          header: {
-            class: Header,
-            shortcut: "CMD+SHIFT+H",
-            config: {
-              placeholder: "Enter a Header",
-            },
-          },
-          list: {
-            class: List,
-            inlineToolbar: true,
-            config: {
-              defaultStyle: "unordered",
-            },
-          },
-          checklist: {
-            class: Checklist,
-            inlineToolbar: true,
-          },
-          paragraph: Paragraph,
-          warning: Warning, // Make sure this is intended or remove if not used
-        },
-        data: fileData?.document ? JSON.parse(fileData.document) : rawDocument,
-      });
-
-      editorInstance.current = editor;
-    };
-
     initEditor();
-
-    return () => {
-      if (editorInstance.current) {
-        editorInstance.current.destroy();
-        editorInstance.current = null;
-      }
-    };
-  }, [fileData, rawDocument]);
+  }, []);
+  const initEditor = () => {
+    const editor = new EditorJS({
+      /**
+       * Id of Element that should contain Editor instance
+       */
+      tools: {
+        header: Header,
+      },
+      holder: "editorjs",
+    });
+  };
 
   return (
     <div>
